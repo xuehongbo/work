5950f574c108c028e2054372318ce9a7fdc9b283
xuehongbo <505386086@qq.com>
2019-12-05 14:00:06 +0800

    new str to dict func

---
+++ caafinder/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='str_dict',
+    version='0.0.1',
+    keywords='ds caa',
+    description='a str to dict tool',
+    license='MIT License',
+    url='https://github.com/Gutier14/CAAFinder',
+    author='HongBo Xue',
+    author_email='505386086@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
