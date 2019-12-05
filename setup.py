b5620fc00a5cbcc23744d7f04fd95ee679d268ed
xuehongbo <505386086@qq.com>
2019-12-05 17:37:49 +0800

    b

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='str_to_dict_tool',
-    version='0.0.2',
+    name='dict_tool',
+    version='0.0.1',
     keywords='str to dict',
     description='a str to dict tool',
     license='MIT License',
     url='https://github.com/xuehongbo/work',
     author='HongBo Xue',
     author_email='505386086@qq.com',
-    packages=find_packages(where="caafinder"),
-    package_dir={'': 'caafinder'},
-    include_package_data=True,
+    packages=find_packages(),
     platforms='any',
     install_requires=[],
 )
